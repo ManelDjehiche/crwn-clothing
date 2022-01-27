74367b80c937589b1903c4c1355d4fe849e38b59
ManelDjehiche <maneldje81@gmail.com>
2022-01-27 15:34:06 +0100

    edit shop page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Route } from 'react-router-dom';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/shop" component={ShopPage}/>
-
+      <Header/>
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </switch>
     </div>
   );
 }
